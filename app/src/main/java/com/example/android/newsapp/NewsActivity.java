b7e048aff4946ae3fedb113b51f72b5a82f48842
Claudia Hill <hillc255@yahoo.com>
2018-08-04 03:33:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -0,0 +1,218 @@
+package com.example.android.newsapp;
+
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Based on ud843-Quake Report - Udemy Android Basics Application
+
+public class NewsActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<News>> {
+
+    private static final String LOG_TAG = NewsActivity.class.getName();
+
+    /**
+     * URL for News app data from the The Guardian
+     */
+    private static final String NEWS_REQUEST_URL =
+            "https://content.guardianapis.com/search";
+           // "https://content.guardianapis.com/search?q=science%20AND%20tech&from-date=2017-01-01&api-key=1d89e06a-a8dc-44de-8eef-b8ef6db55b08&order-by=oldest&show-tags=contributor&show-fields=thumbnail";
+            //"https://content.guardianapis.com/search?q=education%20AND%20science%20AND%20tech%20AND%20cities&from-date=2016-01-01&api-key=1d89e06a-a8dc-44de-8eef-b8ef6db55b08&show-tags=contributor&show-fields=thumbnail";
+           //https://open-platform.theguardian.com/documentation/
+    /**
+     * Constant value for the news loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int NEWS_LOADER_ID = 1;
+
+    /**
+     * Adapter for the list of news items
+     */
+    private NewsAdapter mAdapter;
+
+    /**
+     * TextView that is displayed when the list is empty
+     */
+    private TextView mEmptyStateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_activity);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter that takes an empty list of news items as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsListView.setAdapter(mAdapter);
+
+        //Enable multi-thread for thumbnail
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected news item
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current earthquake that was clicked on
+                News currentNews = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    //Break down URL into URI pieces
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // "https://content.guardianapis.com/search
+        // ?
+        // q=tech&
+        // from-date=2017-01-01&
+        // api-key=
+        // 1d89e06a-a8dc-44de-8eef-b8ef6db55b08&
+        // order-by=oldest&
+        // show-tags=contributor&
+        // show-fields=
+        // thumbnail";
+
+        //getString retrieves String value from preferences - 2nd parameter default value
+//        String minMagnitude = sharedPrefs.getString(
+//                getString(R.string.settings_min_magnitude_key),
+//                getString(R.string.settings_min_magnitude_default));
+//
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+
+        Uri baseUri = Uri.parse(NEWS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("q", "tech");
+        uriBuilder.appendQueryParameter("from-date", "2017-01-01");
+        uriBuilder.appendQueryParameter("api-key", "1d89e06a-a8dc-44de-8eef-b8ef6db55b08");
+        uriBuilder.appendQueryParameter("order-by", "oldest");
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("show-fields", "thumbnail");
+
+        return new NewsLoader(this, uriBuilder.toString());
+    }
+//    @Override
+//    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+//        // Create a new loader for the given URL
+//        return new NewsLoader(this, NEWS_REQUEST_URL);
+//    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> newsList) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No news found."
+        mEmptyStateTextView.setText(R.string.no_news);
+
+        // Clear the adapter of previous news data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (newsList != null && !newsList.isEmpty()) {
+            mAdapter.addAll(newsList);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+    }
+
+    //Preference settings: inflate options menu - the settings for preferences
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //Preference settings: set up action which occurs when option menus selected clicked
+    //a. passes menu item selected
+    //b. options menu may have more than one items
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //determine which item is selected from menu
+        int id = item.getItemId();
+        //match id with known menu items - open settings activity via intent
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -0,0 +1,66 @@
+package com.example.android.newsapp;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+//Prefrences settings
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+    //SETTINGS:  classes implement this interface setup to listen for any Preferene change
+    //made by user
+    public static class EarthquakePreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+
+        //SETTINGS: set up preferences by overriding
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
+            //this was previously commented out
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+
+        //read current value of preference stored in SharedPreferences on device
+        //display summary
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences =
+                    PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+    }
+}
